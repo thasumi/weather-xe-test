a4b53bdba4f819b90458d2ed445d741dfccff568
thaissumiyoshi <thais@frauddeflect.com>
2026-03-03 14:15:56 -0300

    project setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="min-h-screen bg-slate-900 text-white flex items-center justify-center">
+    <h1 class="text-3xl font-bold">
+      Hello Tailwind + Vue 👋
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script> 
